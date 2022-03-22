b17528556870fae855828bb2978c31433f074d4f
Lisa Hansson <lisastina.hansson@gmail.com>
2022-03-22 10:41:59 +0100

    breaking bean animation

--- public/themes/coffe-shop/src/js/bean.js
+++ public/themes/coffe-shop/src/js/bean.js
@@ -3,6 +3,8 @@ import $ from 'jquery';
 $(() => {
   const beans = document.querySelectorAll('.bean');
 
+  const beanImages = ['broken-bean-1', 'broken-bean-2'];
+
   beans.forEach((bean) => {
     bean.addEventListener('click', (e) => {
       if (bean.querySelector('img')) {
@@ -10,21 +12,27 @@ $(() => {
 
         const img = bean.querySelector('img');
 
+        /* Randomize broken bean gif */
+        const randomBean =
+          beanImages[Math.floor(Math.random() * beanImages.length)];
+
         /* if the bean is already broken, replace with whole bean */
-        if (url.includes('broken-bean.png')) {
+        if (url.includes('broken-bean')) {
           img.src =
             url.substring(0, url.lastIndexOf('/') + 1) + img.id + '.png';
           bean.classList.add('wiggle');
+          bean.classList.remove('broken');
         } else {
           /* replacing image src with breaking bean gif */
           img.src =
-            url.substring(0, url.lastIndexOf('/') + 1) + 'broken-bean-2.gif';
+            url.substring(0, url.lastIndexOf('/') + 1) + randomBean + '.gif';
           bean.classList.remove('wiggle');
+          bean.classList.add('broken');
 
           /* no looping of gif when clicking other beans */
           setTimeout(() => {
             img.src =
-              url.substring(0, url.lastIndexOf('/') + 1) + 'broken-bean.png';
+              url.substring(0, url.lastIndexOf('/') + 1) + randomBean + '.png';
           }, 500);
         }
       }
